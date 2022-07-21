288feabf68df9cdbf3f84bd1d456417a36ceb2d3
kjkj2077 <kjkj2077@naver.com>
2022-07-22 02:21:29 +0900

    weater_api 

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import { useEffect,useState} from 'react';
 import './App.css';
 
+
 function App() {
+  const getCurrentLocation = ()=>{
+    navigator.geolocation.getCurrentPosition((position)=>{
+        let lat = position.coords.latitude;
+        let lon = position.coords.longitude;
+        getWeatherByCurrentLocation(lat,lon)
+    });
+  }
+  const getWeatherByCurrentLocation = async(lat,lon)=>{
+    let url =`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=2bbfca26b218d02919ec5a1c0f56539a`
+    let resopon= await fetch(url) //fetch하는걸 기다려주셈
+    let data = await resopon.json();
+    console.log("data",data)
+  }
+
+  useEffect(()=>{ //렌더후 바로실행 
+    getCurrentLocation()
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      hello
     </div>
   );
 }
